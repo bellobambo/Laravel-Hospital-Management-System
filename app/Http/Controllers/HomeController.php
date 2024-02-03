0a333beaa5e21870c7ca78c62b13616e4ae3cce2
bellobambo <bellobambo21@gmail.com>
2024-02-03 12:40:00 +0100

    add appointment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
+use App\Models\Doctor;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,8 +14,10 @@ class HomeController extends Controller
     {
         if (Auth::id()) {
 
-            if (Auth::user()->userType=='0') {
-                return view('user.home');
+            if (Auth::user()->userType == '0') {
+                $doctor = Doctor::all();
+
+                return view('user.home', compact('doctor'));
             } else {
                 return view('admin.home');
             }
@@ -24,7 +28,34 @@ class HomeController extends Controller
     }
 
 
-    public function index(){
-        return view('user.home');
+    public function index()
+    {
+        if(Auth::id()){
+            return redirect('home');
+        }else{
+            $doctor = Doctor::all();
+            return view('user.home', compact('doctor'));
+
+        }
+    }
+
+    public function appointment(Request $request){
+
+        $data = new Appointment;
+
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->date = $request->date;
+        $data->phone = $request->phone;
+        $data->message = $request->name;
+        $data->doctor = $request->doctor;
+        $data->status = 'In Progress';
+        if(Auth::id()){
+            $data->user_id = Auth::user()->id;
+        }
+
+        $data->save();
+
+        return redirect()->back()->with('message', 'Appointment Successful');
     }
 }
